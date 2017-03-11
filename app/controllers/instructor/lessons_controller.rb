85fb063cab57b9e5ed7f9eca68975f3cb3975436
DrKalen <kalen7@gmail.com>
2017-03-11 00:31:07 +0000

    Added code for permissions in lessons controller but not working

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,11 +1,22 @@
 class Instructor::LessonsController < ApplicationController
+    before_action: authenticate_user
+
     def new
         @section = Section.find(params[:section_id])
+          if @section.course.user != current_user
+      return render text: 'Unauthorized', status: :unauthorized
+    end
+        if @section.course.user != current_user
+            return render text: 'Unauthorized', status: :Unauthorized
+        end    
         @lesson = Lesson.new
     end
 
     def create
         @section = Section.find(params[:section_id])
+        if @section.course.user != current_user
+            return render text: 'Unauthorized', status: :Unauthorized
+        end
         @lesson = @section.lessons.create(lesson_params)
         redirect_to instructor_course_path(@section.course)
     end
