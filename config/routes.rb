fb9fa7f40bf08c46d58281e0795815e7c05b4f8c
DrKalen <kalen7@gmail.com>
2017-03-17 21:02:32 +0000

    Added enrollment and You Are Enrolled functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
         resources :enrollments, only: :create 
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
@@ -10,7 +11,6 @@ Rails.application.routes.draw do
     end
     resources :courses, only: [:new, :create, :show] do
         resources :sections, only: [:new, :create] 
-        end
     end
   end
 end
