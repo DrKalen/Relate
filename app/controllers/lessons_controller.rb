94339ddf6b146602747824a3352a080470bd7680
DrKalen <kalen7@gmail.com>
2017-03-18 13:55:46 +0000

    Tried to add permissions to view the lessons only if enrolled - error message.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_authorized_for_current_course, only: [:show]
 
     def show
     end
 
     private
+
+    def authorized_for_current_course
+        if current_lesson.section.course.user !=current_user && !current_user.enrolled_in?(current_lesson.section.course)
+            redirect_to course_path(current_lesson.section.course), alert: ‘You’ll have access to this lesson as soon as you enroll in the course.’
+        end
+    end
 
     helper_method :current_lesson
     def current_lesson
